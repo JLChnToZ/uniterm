5f55bc5e4f6adaf9a3f281a379f7f53e037e4477
JLChnToZ <jeremylam619@gmail.com>
2018-11-21 16:31:08 +0800

    Add helper script for use in WSL environment

--- .packager.js
+++ .packager.js
@@ -1,5 +1,7 @@
 const packager = require('electron-packager');
 const { rebuild } = require('electron-rebuild');
+const { writeFile, existsSync } = require('fs');
+const { resolve } = require('path');
 
 packager({
   dir: __dirname,
@@ -15,11 +17,11 @@ packager({
     CompanyName: 'Explosive Theorem Lab.'
   },
   asar: {
-    unpackDir: 'node_modules/{node-pty,wslpty}/**'
+    unpackDir: 'node_modules/**/{R,r}elease'
   },
   ignore: [
     /\.([a-z0-9]*ignore|gypi?|sln|pdb|md|t?log|cmd|bat|sh|ps1|lib|exp|map|cc|h|tsx?|sass|coffee)$/i,
-    /[\\\/](deps|tests?|example)[\\\/]/i,
+    /[\\\/](deps|tests?|example|bin)[\\\/]/i,
     /\.gitmodules/i,
     /ts(config|lint).json$/i,
     /\.[a-z]+proj(\.filters)?$/i,
@@ -28,8 +30,27 @@ packager({
     /(^|[\\\/])\.[^\\\/]*$/i
   ],
   afterCopy: [
-    (buildPath, electronVersion, platform, arch, callback) =>
-      rebuild({ buildPath, electronVersion, arch })
+    (buildPath, electronVersion, platform, arch, callback) => {
+      console.log('Rebuild native modules...');
+      rebuild({
+        buildPath,
+        electronVersion,
+        arch,
+      })
       .then(() => callback(), err => callback(err))
+    },
+    (buildPath, electronVersion, platform, arch, callback) => {
+      if(platform !== 'win32')
+        return callback();
+      const path = resolve(buildPath, '../../uniterm');
+      if(existsSync(path))
+        return callback();
+      console.log('Create helper script for use in WSL...');
+      writeFile(path,
+        '#!/usr/bin/env sh\nuniterm.exe $@\n',
+        'utf-8',
+        callback
+      );
+    },
   ],
 });
