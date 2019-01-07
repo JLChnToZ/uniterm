a5881895751af89618578c73563f3ed9ea3283c0
JLChnToZ <jeremylam619@gmail.com>
2019-01-07 18:38:30 +0800

    Reduce size

--- .packager.js
+++ .packager.js
@@ -27,9 +27,10 @@ packager({
   },
   ignore: [
     /\.([a-z0-9]*ignore|sln|pdb|md|t?log|ps1|lib|exp|map|tsx?|sass|coffee)$/i,
-    /[\\\/](tests?|example|bin|obj|build)[\\\/]/i,
+    /[\\\/](tests?|example|bin|obj|build|@types)([\\\/]|$)/i,
     /\.gitmodules/i,
     /ts(config|lint).json$/i,
+    /yarn.lock$/i,
     /\.[a-z]+proj(\.filters)?$/i,
     /\.packager\.js$/i,
     /readme[^\\\/]*$/i,
